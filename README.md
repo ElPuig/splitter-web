66fc4c911dce7920f26f660b977ab9272ed6bf97
marcos <marcosalcocer@gmail.com>
2021-01-09 00:36:13 +0100

    Commit inicial. Refactorització de l'aplicació original a la qual la compressió dels fitxers temporals generats per el script de Python, així com la seva eliminació, passa a estar a càrrec del propi script.

---
+++ README.md
@@ -0,0 +1,38 @@
+# splitter-web
+Split SAGA based PDFs reports into student individual PDFs.
+
+## Installation
+Clone the repository into your web root folder.
+
+Make sure `uploads/` and `batch/tmp/` directories are web writable.
+
+## Requirements
+	 sudo apt-get install python3
+	
+	 sudo apt-get install python3-pypdf2
+
+## apache2 configuration: Virtual Host
+
+	 <VirtualHost *:80>
+		 ServerAdmin admin@splitter.local
+		 ServerName www.splitter.local
+		 ServerAlias splitter.local
+		 DocumentRoot /var/www/splitter.local/public
+		 ErrorLog ${APACHE_LOG_DIR}/error.log
+		 CustomLog ${APACHE_LOG_DIR}/access.log combined
+	 </VirtualHost>
+
+## How to use the splitter without web
+
+Into the `batch/` directory you will find two scripts:
+
+	ButlletinsSplitter.py
+
+	SagaSplit.sh
+
+Choose one according to your needs, being SagaReport.pdf the report that you have downloaded from Saga.
+
+	python3 ButlletinsSplitter.py SagaReport.pdf
+
+	sh SagaSplit.sh SagaReport.pdf
+
---
+++ batch/ButlletinsSplitter.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ButlletinsSplitter1.3.py
+Fitxer d'entrada:     Fitxer PDF amb els butlletins de notes d'un grup sencer
+                      amb el nom «informe.pdf», preparat o no per la impressió
+                      a doble cara.
+Fitxers de sortida:   Un fitxer ZIP amb el butlletí de cada estudiant en un
+                      arxiu PDF individual i reanomenat amb el seu nom.
+"""
+
+import glob
+import os
+import sys
+import PyPDF2
+from zipfile import ZipFile
+
+SOURCE_FILE_SAGA = sys.argv[1]
+RESULT_ZIP_FILE = 'butlletins.zip'
+TMP_DIR = '../batch/tmp'
+
+
+"""def generate_individual_files(s)
+Descripció: Divideix el PDF amb tots els butlletins de notes
+            en PDF amb els butlletins individuals per estudiant.
+Entrada:    Cap.
+Sortida:    Cap.
+"""
+def generate_individual_files():
+    butlletins = open(SOURCE_FILE_SAGA, 'rb')
+    butlletins_reader = PyPDF2.PdfFileReader(butlletins)
+
+    total_pages = butlletins_reader.getNumPages()
+    page_num = 0
+    current_student = None
+
+    # Iterates through whole PDF source file
+    while page_num < total_pages:
+        if current_student is None:
+            page_text = butlletins_reader.getPage(page_num)
+            page_text_string = page_text.extractText()
+            current_student = get_student_name(page_text_string)
+
+        student = current_student
+        output_file = (os.path.join(TMP_DIR, student + '.pdf'))
+        output_writer = PyPDF2.PdfFileWriter()
+
+        # Iterates through new individually created PDF file
+        while (current_student == student and
+               current_student is not None and
+               page_num <= total_pages-1):
+            output_writer.addPage(page_text)
+
+            page_num += 1
+
+            if page_num < total_pages:
+                page_text = butlletins_reader.getPage(page_num)
+                page_text_string = page_text.extractText()
+                current_student = get_student_name(page_text_string)
+
+                # Blank page
+                if current_student is None:
+                    page_num += 1
+
+            with open(output_file, 'wb') as output_stream:
+                output_writer.write(output_stream)
+
+    butlletins.close()
+
+
+"""def get_student_name(s)
+Descripció: Troba el nom de l'alumne al text d'una pàgina del butlletí de
+            notes passat com a string.
+Entrada:    String.
+Sortida:    String amb el nom de l'alumne si la pàgina no està buida;
+            None en cas contrari.
+"""
+def get_student_name(s):
+
+    try:
+        # Get student's name
+        """ start: 'Grup' (al text sempre davant del nom de l'alumne
+                           i el seu DNI)
+            end:   'CFP' (al text sempre darrere del nom de l'alumne
+                          i el seu DNI)
+        """
+        start_index = s.index('Grup') + len('Grup')
+        end_index = s.index('CFP')
+        # Ignore ID's last char
+        student_with_id = s[start_index:end_index][:-1]
+
+        # Remove ID from substring (every number and/or
+        #                           letter followed by a number)
+        student = ''.join(l for i, l in enumerate(student_with_id)
+                          if not l.isdigit() and not
+                          (l.isupper() and student_with_id[i+1].isdigit()))
+
+        return student
+    except:  # Blank page
+        return None
+
+
+"""def zip_files
+Descripció: Comprimeix tots els butlletins PDF individuals en un únic
+            fitxer ZIP.
+Entrada:    Cap.
+Sortida:    Cap.
+"""
+def zip_files():
+    pdf_files_list = [os.path.join(os.getcwd(), TMP_DIR, f) for f
+                      in os.listdir(os.path.join(os.getcwd(),TMP_DIR))
+                      if f.split('.')[1]=='pdf']
+
+    result_zip_file = os.path.join(os.getcwd(),TMP_DIR, RESULT_ZIP_FILE)
+
+    with ZipFile(result_zip_file, 'w') as zip:
+        for file in pdf_files_list:
+            file_name = file.split('/')[-1]
+            zip.write(file, file_name)
+
+
+"""def remove_tmp_files
+Descripció: Suprimeix tots els butlletins PDF individuals.
+Entrada:    Cap.
+Sortida:    Cap.
+"""
+def remove_tmp_files():
+    pdf_files_list = [os.path.join(os.getcwd(),TMP_DIR, f) for f
+                      in os.listdir(os.path.join(os.getcwd(),TMP_DIR))
+                      if f.split('.')[1]=='pdf']
+
+    for file in pdf_files_list:
+        os.remove(file)
+
+
+if __name__ == '__main__':
+    generate_individual_files()
+    zip_files()
+    remove_tmp_files()
